6fdcb67b359b516fa974329ee562c423609caff3
DmGsOuNaK <sounak.swamiji@gmail.com>
2023-06-15 01:28:37 +0530

    my-portfolio

---
+++ src/Constants/constants.js
@@ -0,0 +1,206 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faX,
+  faBars,
+  faWindowRestore,
+  faBagShopping,
+  faDiceD6,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faReact,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+
+import {
+  htmlIcon,
+  cssIcon,
+  jsIcon,
+  reactIcon,
+  javaIcon,
+  gitIcon,
+  githubIcon,
+  pyIcon,
+  nodeIcon,
+  figmaIcon,
+  pawsitivePrototype,
+  calculator,
+  memoryGame,
+  avatar,
+} from '../assets';
+
+library.add(faX, faBars, faWindowRestore, faBagShopping, faDiceD6);
+
+const media = {
+  htmlIcon,
+  cssIcon,
+  jsIcon,
+  reactIcon,
+  javaIcon,
+  gitIcon,
+  githubIcon,
+  pyIcon,
+  nodeIcon,
+  figmaIcon,
+  avatar,
+};
+
+const icons = {
+  faBars,
+  faX,
+  faWindowRestore,
+  faBagShopping,
+  faDiceD6,
+  faReact,
+  faGithub,
+  faLinkedin,
+  faEnvelope,
+};
+
+const introduction = {
+  text: [
+    "Hello there, I'm so happy you are here! (:",
+
+    "My name is Sounak Mandal, I'm 21 and a forth year computer science student based from VIT,vellore. I have a strong passion for the technologies related to the web development",
+
+    "Outside of my regular activities, I love to play guitar whenever I have some free time. I'm also a huge cricket lover and enjoy playing chess for fun.",
+
+    "I'm continuously thrilled to partner with individuals and teams who wholeheartedly embrace my fervor for crafting extraordinary experiences. Your visit to my portfolio is deeply appreciated !",
+  ],
+};
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'skills',
+    title: 'Skills',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const projects = [
+  {
+    name: 'Library Management System',
+    description: 'It helps users to perform CRUD operations. In addition to the advanced search, book issue , Serialisation and Deserialisation',
+    image: memoryGame,
+    source_code_link: 'https://github.com/DmGsOuNaK/Library-Management-',
+    demo_link: 'https://github.com/DmGsOuNaK/Library-Management-',
+  },
+  {
+    name: 'Split and Happy',
+    description: 'It scans handwritten bills and split it among the participant.',
+    image: pawsitivePrototype,
+    source_code_link: 'https://github.com/DmGsOuNaK/Split-and-Happy',
+    demo_link: 'https://github.com/DmGsOuNaK/Split-and-Happy',
+  },
+  {
+    name: 'Banking Chatbot',
+    description: 'Can answer any kind of bank related queries',
+    image: calculator,
+    source_code_link: '#',
+    demo_link: '#',
+  },
+];
+
+const memoji = {
+  image: [avatar],
+};
+
+const skills = [
+  {
+    id: 'html',
+    title: 'HTML',
+    icon: htmlIcon,
+    description:
+      'I have a strong command of HTML for organizing web pages and generating meaningful content that can be accessed by all users.',
+  },
+  {
+    id: 'css',
+    title: 'CSS',
+    icon: cssIcon,
+    description:
+    'I possess expertise in utilizing CSS to design web pages and craft visually captivating layouts that enhance the overall user experience.',
+  },
+  {
+    id: 'javascript',
+    title: 'JavaScript',
+    icon: jsIcon,
+    description:
+    'I have substantial experience in employing JavaScript to introduce interactivity and functionality into web pages, resulting in dynamic user interfaces.',
+  },
+  {
+    id: 'react',
+    title: 'React',
+    icon: reactIcon,
+    description:
+      'I am well-versed in React, proficient in creating reusable components and managing application state using hooks and context.',
+  },
+  {
+    id: 'java',
+    title: 'Java',
+    icon: javaIcon,
+    description:
+      'I have extensive experience utilizing Java for object-oriented programming (OOP) and implementing data structures.',
+  },
+  {
+    id: 'figma',
+    title: 'Figma',
+    icon: figmaIcon,
+    description:
+    'In my Figma skills, I unleash creativity, designing captivating user interfaces and collaborating seamlessly with designers and developers.',
+  },
+  {
+    id: 'git',
+    title: 'Git',
+    icon: gitIcon,
+    description:
+    'I am proficient in Git, managing code changes, collaborating with others, and resolving conflicts effectively.',
+  },
+  {
+    id: 'github',
+    title: 'GitHub',
+    icon: githubIcon,
+    description:
+      'I am skilled in using GitHub for seamless project collaboration, code sharing, and issue tracking. Through GitHub, I efficiently create and manage repositories and effectively present my work to potential employers.',
+  },
+  {
+    id: 'py',
+    title: 'Python',
+    icon: pyIcon,
+    description:
+      'With 3 years of Python experience, I am adept at coding functions and creating graphic interfaces using Tkinter.',
+  },
+  {
+    id: 'node',
+    title: 'Node',
+    icon: nodeIcon,
+    description:
+      'When it comes to building web applications, I prefer using Node as my runtime environment over Yarn. I have expertise in leveraging Node.js to develop powerful and scalable web applications.',
+  },
+];
+
+const markerSvg = `<svg viewBox="-4 0 36 36">
+    <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
+    <circle fill="black" cx="14" cy="14" r="7"></circle>
+  </svg>`;
+
+export {
+  media,
+  introduction,
+  projects,
+  memoji,
+  skills,
+  markerSvg,
+  icons,
+};
